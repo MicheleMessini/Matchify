0b281e187df0648ec88e8c93d6fa00eb3f5d7ea0
MicheleMessini <mickymessi07@icloud.com>
2025-05-17 12:46:19 +0200

    Update server.js

--- Desktop/spotify-vinili/server.js
+++ Desktop/spotify-vinili/server.js
@@ -374,7 +374,8 @@ app.get('/album/:id', async (req, res) => {
       </head>
       <body>
         <div class="container">
-          <h1>${album.name} - ${album.artists.map(a => a.name).join(', ')}</h1>
+          <h1>${album.name} - </h1>
+          <h1>${album.artists.map(a => a.name).join(', ')}</h1>
           <img src="${album.images[0]?.url || ''}" alt="${album.name}" style="max-width: 500px;" />
           <ol>
             ${albumTracks.map(track => {
