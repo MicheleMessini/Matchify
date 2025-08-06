b9e6de30234638e3e95a6a29fa0b5b94e0ef0e41
MicheleMessini <mickymessi07@icloud.com>
2025-08-07 00:00:18 +0200

    Update cache.js

--- Desktop/spotify-vinili/utils/cache.js
+++ Desktop/spotify-vinili/utils/cache.js
@@ -1,1 +1,7 @@
+const NodeCache = require('node-cache');
 
+// Crea una singola istanza della cache con una durata standard (TTL) di 15 minuti.
+// Esportando l'istanza, ti assicuri che sia la stessa in tutta l'applicazione.
+const appCache = new NodeCache({ stdTTL: 900 });
+
+module.exports = appCache;
