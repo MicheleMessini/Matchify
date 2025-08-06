4c5b62827a793d75c869821c13eebbb709859fb4
MicheleMessini <mickymessi07@icloud.com>
2025-08-06 23:59:53 +0200

    Update apiRoutes.js

--- Desktop/spotify-vinili/routes/apiRoutes.js
+++ Desktop/spotify-vinili/routes/apiRoutes.js
@@ -1,1 +1,11 @@
+const express = require('express');
+const { getPlaylistDuration } = require('../controllers/apiController');
+const router = express.Router();
 
+/**
+ * Rotta API per ottenere la durata di una playlist.
+ * Risponde a GET /api/duration/:playlistId
+ */
+router.get('/duration/:playlistId', getPlaylistDuration);
+
+module.exports = router;
