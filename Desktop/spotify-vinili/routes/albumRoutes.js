b68d8ccd70d707d759726b3a3a33520070885d70
MicheleMessini <mickymessi07@icloud.com>
2025-08-06 23:58:54 +0200

    Update albumRoutes.js

--- Desktop/spotify-vinili/routes/albumRoutes.js
+++ Desktop/spotify-vinili/routes/albumRoutes.js
@@ -1,1 +1,9 @@
+const express = require('express');
+const { getAlbumDetails } = require('../controllers/albumController');
+const router = express.Router();
 
+// Il percorso qui è relativo a dove monti il router in server.js
+// Se lo monti con app.use('/album', albumRoutes), questo corrisponderà a /album/:id
+router.get('/:id', getAlbumDetails);
+
+module.exports = router;
